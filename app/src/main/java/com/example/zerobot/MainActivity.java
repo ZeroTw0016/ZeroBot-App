c2744e29d3af802433296f4d218f9deb834afc11
ZeroTw0016 <84594740+ZeroTw0016@users.noreply.github.com>
2022-12-19 15:37:12 +0100

    created
    .

---
+++ app/src/main/java/com/example/zerobot/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.zerobot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://music-bot.zerotwo.eu.org");
+    }
+}
